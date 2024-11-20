09f9a42de634fa30e3371a91b7e6b668e4303450
dynann <dynannran@gmail.com>
2024-11-21 06:53:25 +0700

    fix phone otp

--- components/Auth/login/loginphone/page.js
+++ components/Auth/login/loginphone/page.js
@@ -22,11 +22,11 @@ export default function LoginPhone() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ phone: phoneNumber }), // Fixed the key
+        body: JSON.stringify({ phone: phoneNumber }),
       });
 
       if (res.ok) {
-        setVerify(true); // Move to OTP input
+        setVerify(true); 
       } else {
         const errorData = await res.json();
         setError(errorData.message);
@@ -52,11 +52,13 @@ export default function LoginPhone() {
       if (res.ok) {
         const phone = "0123456789";
         const otp = "123456";
-        await signIn('phoneCredentials', { phone,
+        await signIn('phoneCredentials', { 
+          phone,
           otp,
           redirect: false,
-          callbackUrl: "/", })
-        router.push('/')
+          callbackUrl: "/", 
+        });
+        router.push('/');
       } else {
         const errorData = await res.json();
         setError(errorData.message);
@@ -67,56 +69,54 @@ export default function LoginPhone() {
   };
 
   return (
-    <div className="flex flex-col lg:flex-row h-screen w-full overflow-hidden">
-      <div className="w-full lg:w-6/12 h-auto p-4 lg:p-10">
-        <div className="flex flex-col justify-center items-center h-full">
+    <div className="flex justify-center items-center h-screen">
+      <div className="w-full max-w-md bg-white p-8 rounded-lg shadow-lg">
+        <div className="text-center mb-6">
           <h1 className="text-black font-bold text-2xl md:text-3xl lg:text-4xl">
             Login with Phone Number
           </h1>
-          <div className="mt-10 flex flex-col justify-center w-9/12">
-            <form
-              className="flex flex-col mb-8"
-              onSubmit={verify ? handleOtpVerify : handlePhoneVerify}
-            >
-              {!verify ? (
-                <>
-                  <label htmlFor="phone" className="mb-2 font-bold">
-                    Phone Number
-                  </label>
-                  <input
-                    className="border-2 p-2 rounded-xl border-black mb-6"
-                    type="tel"
-                    value={phoneNumber}
-                    required
-                    onChange={(e) => setPhoneNumber(e.target.value)}
-                    placeholder="Enter phone number"
-                  />
-                </>
-              ) : (
-                <>
-                  <label htmlFor="otp" className="mb-2 font-bold">
-                    OTP
-                  </label>
-                  <input
-                    className="border-2 p-2 rounded-xl border-black mb-6"
-                    type="text"
-                    value={otp}
-                    required
-                    onChange={(e) => setOtp(e.target.value)}
-                    placeholder="Enter OTP"
-                  />
-                </>
-              )}
-              <button
-                type="submit" // Changed type to "submit"
-                className="p-3 border-none rounded-xl bg-customPurple-default hover:bg-customPurple-hover text-center text-white"
-              >
-                {verify ? "Verify OTP" : "Send OTP"}
-              </button>
-              {error && <p className="text-red-500 mt-4">{error}</p>}
-            </form>
-          </div>
         </div>
+        <form
+          className="flex flex-col"
+          onSubmit={verify ? handleOtpVerify : handlePhoneVerify}
+        >
+          {!verify ? (
+            <>
+              <label htmlFor="phone" className="mb-2 font-bold">
+                Phone Number
+              </label>
+              <input
+                className="border-2 p-2 rounded-xl border-black mb-6"
+                type="tel"
+                value={phoneNumber}
+                required
+                onChange={(e) => setPhoneNumber(e.target.value)}
+                placeholder="Enter phone number"
+              />
+            </>
+          ) : (
+            <>
+              <label htmlFor="otp" className="mb-2 font-bold">
+                OTP
+              </label>
+              <input
+                className="border-2 p-2 rounded-xl border-black mb-6"
+                type="text"
+                value={otp}
+                required
+                onChange={(e) => setOtp(e.target.value)}
+                placeholder="Enter OTP"
+              />
+            </>
+          )}
+          <button
+            type="submit"
+            className="p-3 border-none rounded-xl bg-customPurple-default hover:bg-customPurple-hover text-center text-white"
+          >
+            {verify ? "Verify OTP" : "Send OTP"}
+          </button>
+          {error && <p className="text-red-500 mt-4">{error}</p>}
+        </form>
       </div>
     </div>
   );

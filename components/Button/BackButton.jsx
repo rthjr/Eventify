190ff1a2a4287dd7ef00aac18be9faed91fc28e9
Potato <137735702+rthjr@users.noreply.github.com>
2024-12-04 15:39:20 +0700

    component(Button, BackButton)

--- components/Button/BackButton.jsx
+++ components/Button/BackButton.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
-const BackButton = ({ param }) => {
+const BackButton = ({ param, onClick }) => {
   return (
-    <button className="btn border-b-2 border-b-black hover:border-b-customPurple-hover btn-xs sm:btn-sm md:btn-md lg:btn-lg ">
+    <button
+      className="btn border-b-2 border-b-black hover:border-b-customPurple-hover btn-xs sm:btn-sm md:btn-md lg:btn-lg "
+      onClick={onClick}
+    >
       {param}
     </button>
   );

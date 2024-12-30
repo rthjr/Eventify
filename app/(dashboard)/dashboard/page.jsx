b3062f69b09c27fc32122868030af99e6db44975
dynann <dynannran@gmail.com>
2024-12-30 15:56:29 +0700

    add user role based

--- app/(dashboard)/dashboard/page.jsx
+++ app/(dashboard)/dashboard/page.jsx
@@ -1,8 +1,10 @@
 import DashboardAdmin from "@components/util/Dashboard";
 export default function DashBoard() {
+    
     return(
-        <div>
-            <DashboardAdmin/>
-        </div>
+        
+       <div>
+        <DashboardAdmin/>
+       </div>
     )
 }
